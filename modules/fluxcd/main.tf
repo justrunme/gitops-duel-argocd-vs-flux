91ab631f40adffbafb6c0beb4a31c913fef3e827
justrunme <justrunme@gmail.com>
2025-06-28 11:24:23 +0200

    Fix: Flux CLI execution error in CI

--- modules/fluxcd/main.tf
+++ modules/fluxcd/main.tf
@@ -8,19 +8,19 @@ terraform {
 
 resource "null_resource" "flux_install" {
   provisioner "local-exec" {
-    command = "./bin/flux install --namespace flux-system"
+    command = "flux install --namespace flux-system"
   }
 }
 
 resource "null_resource" "flux_sync" {
   provisioner "local-exec" {
     command = <<EOT
-      ./bin/flux create source git local-repo \
+      flux create source git local-repo \
         --url=https://github.com/justrunme/gitops-duel-argocd-vs-flux.git \
         --branch=main \
         --namespace=flux-system
 
-      ./bin/flux create kustomization nginx \
+      flux create kustomization nginx \
         --source=GitRepository/local-repo \
         --path=./apps/flux/nginx \
         --prune=true \
