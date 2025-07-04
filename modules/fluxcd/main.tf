9c903f61e2959da0389a6c040b96e04a82dfe4b3
justrunme <justrunme@gmail.com>
2025-07-04 13:00:04 +0200

    Fix Terraform CRD timing issues and apply infrastructure

--- modules/fluxcd/main.tf
+++ modules/fluxcd/main.tf
@@ -6,8 +6,8 @@ terraform {
     helm = {
       source = "hashicorp/helm"
     }
-    null = {
-      source = "hashicorp/null"
+    kubectl = {
+      source = "gavinbunney/kubectl"
     }
   }
 }
@@ -56,50 +56,37 @@ resource "helm_release" "flux" {
   }]
 }
 
-resource "null_resource" "flux_crds_ready" {
+resource "kubectl_manifest" "flux_git_repository" {
   depends_on = [helm_release.flux]
-  provisioner "local-exec" {
-    command = "kubectl wait --for=condition=Established crd/kustomizations.kustomize.toolkit.fluxcd.io --timeout=120s"
-  }
+  yaml_body = <<-EOT
+    apiVersion: source.toolkit.fluxcd.io/v1
+    kind: GitRepository
+    metadata:
+      name: local-repo
+      namespace: ${var.flux_namespace}
+    spec:
+      interval: 1m
+      url: ${var.git_repository_url}
+      ref:
+        branch: ${var.git_branch}
+  EOT
 }
 
-resource "kubernetes_manifest" "flux_git_repository" {
-  depends_on = [null_resource.flux_crds_ready]
-  manifest = {
-    apiVersion = "source.toolkit.fluxcd.io/v1"
-    kind       = "GitRepository"
-    metadata = {
-      name      = "local-repo"
-      namespace = var.flux_namespace
-    }
-    spec = {
-      interval = "1m"
-      url      = var.git_repository_url
-      ref = {
-        branch = var.git_branch
-      }
-    }
-  }
-}
-
-resource "kubernetes_manifest" "flux_kustomization" {
-  depends_on = [kubernetes_manifest.flux_git_repository]
-  manifest = {
-    apiVersion = "kustomize.toolkit.fluxcd.io/v1"
-    kind       = "Kustomization"
-    metadata = {
-      name      = "flux-nginx-app"
-      namespace = var.flux_namespace
-    }
-    spec = {
-      interval = "1m"
-      path     = "./apps/flux/nginx"
-      prune    = true
-      targetNamespace = var.target_namespace
-      sourceRef = {
-        kind = "GitRepository"
-        name = "local-repo"
-      }
-    }
-  }
+resource "kubectl_manifest" "flux_kustomization" {
+  depends_on = [kubectl_manifest.flux_git_repository]
+  yaml_body = <<-EOT
+    apiVersion: kustomize.toolkit.fluxcd.io/v1
+    kind: Kustomization
+    metadata:
+      name: flux-nginx-app
+      namespace: ${var.flux_namespace}
+    spec:
+      interval: 1m
+      path: "./apps/flux/nginx"
+      prune: true
+      targetNamespace: ${var.target_namespace}
+      sourceRef:
+        kind: GitRepository
+        name: local-repo
+  EOT
 }
